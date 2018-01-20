064b27173da8800143cd7363d9eda88b5bbb329f
Suryaveer <1998sveer@gmail.com>
2018-01-21 00:48:56 +0530

    adding retwet.py

--- retweet.py
+++ retweet.py
@@ -1,17 +1,22 @@
+#####################################################
+#############  By: Suryaveer @IIT indore  ###########
+#############  Handle @ayrusreev          ###########
+#####################################################
 
 import tweepy
 import time
 
-consumer_key = '8ROBF6oXfqKojxSGSPQn9bjBC'
-consumer_secret = 'TG8IZiVVsWkAvAW0IuWi7UvQVP4Spkmdh4NawVxpReJQzUfrcM'
-access_token = '3241977882-vC0oeIcjrBIU7oRjWLxy3hU35ntsuU7XNBPXWQN'
-access_token_secret = 'Xkyzls3T0rD2T5l67o3hMfyQkGWFRzgZYH9Wnkp92m7iO'
+consumer_key = '********'
+consumer_secret = '***********'
+access_token = '************'
+access_token_secret = '**********'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-hash_tags = ['#IIT_indore','#not_so_prestigious','#bhookhLagiHai ']
+#you can add more tags.. in hash_tag list
+hash_tags = ['#example1','#example2','#example3']
 
 for query in hash_tags:
     for tweet in tweepy.Cursor(api.search, q=query).items(5):
@@ -23,7 +28,8 @@ for query in hash_tags:
             print('Retweet published successfully.')
             time.sleep(10)
         except tweepy.TweepError as error:
-            print('\nError. Retweet not successful. Reason: ')
+            print('\nError. Unable to tweet:')
             print(error.reason)
+            
         except StopIteration:
             break
